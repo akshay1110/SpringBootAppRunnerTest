f16802f11f14a5e7658612817a2deacad8a4a1bb
user <user@192.168.0.6>
2019-06-09 17:14:13 +0530

    assignment no 5 completed...

---
+++ src/main/java/com/app/runner/AppRunner.java
@@ -0,0 +1,18 @@
+package com.app.runner;
+
+import java.util.Arrays;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner{
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+        String[] arr=args.getSourceArgs();
+		System.out.println(Arrays.asList(arr));
+	}
+
+}
